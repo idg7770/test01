f3e515ea43305ad0ad4d06a61acfc23b5f96273b
id <id@AQ-0018>
2023-06-21 15:34:46 +0900

    一行追加

--- src/p01/sample01.java
+++ src/p01/sample01.java
@@ -5,7 +5,7 @@ public class sample01 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("山田太郎");
-		System.out.println("山田次郎");
+		System.out.println("22歳");
 	}
 
 }
